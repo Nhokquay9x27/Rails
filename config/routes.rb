81715cc290ee656cc6de600a9dfaf6c4142810ed
Nhokquay9x26 <nhokquay9x26@Nhokquay9x26s-MacBook-Pro.local>
2018-11-13 18:06:10 +0700

    filling in layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # root 'static_page#home'
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
